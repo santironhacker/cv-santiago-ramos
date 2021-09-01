ca516bd40f45f69b21166e0274b4e1565c28acfb
santironhacker <santi92.network@gmail.com>
2021-09-01 19:05:29 +0700

    Moved project files into src folder.

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 (function () {
     function init() {
         var router = new Router([
-            new Route('home', 'home.html', true),
-            new Route('education', 'education.html'),
-            new Route('positions', 'positions.html'),
-            new Route('projects', 'projects.html'),
-            new Route('tools', 'tools.html'),
+            new Route("home", "home.html", true),
+            new Route("education", "education.html"),
+            new Route("positions", "positions.html"),
+            new Route("projects", "projects.html"),
+            new Route("tools", "tools.html"),
         ]);
     }
     init();
-}());
+})();
 
 // TO DO --for animation of images
 // function home()
